ee15792b92a63e2eee1b8cf3b728f965823bceb1
Sebastian <106036+schoeller@users.noreply.github.com>
2024-12-09 17:36:17 +0100

    Update commands.py

--- commands.py
+++ commands.py
@@ -2,20 +2,22 @@ import traceback
 from pyrx_imp import Ap, Db, Ed, Ge, Gi, Gs, Rx, Bim
 
 from helper import helper
+from helper import repo
 
 print("added command - bct_test")
 print("added command - bct_reloadmodules")
+print("added command - bct_repoupdate")
 
 # On PYRELOAD reload all used custom modules
 def OnPyReload() -> None:
     try:
         import importlib
         importlib.reload(helper)
+        importlib.reload(repo)
         print("\nReloading all modules")
     except Exception as err:
         print(err)
 
-# define a command
 def PyRxCmd_bct_test():
     try:
         print("roks")
@@ -25,5 +27,11 @@ def PyRxCmd_bct_test():
 def PyRxCmd_bct_reloadmodules():
     try:
         print("#TODO: initialize OnPyReload on commands")
+    except Exception as err:
+        print(err)
+
+def PyRxCmd_bct_repoupdate():
+    try:
+        repo.pull_git_changes()
     except Exception as err:
         print(err)
