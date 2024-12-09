dd3664a41ed9d325bc14209ed0a97e27c465caad
Sebastian <106036+schoeller@users.noreply.github.com>
2024-12-09 18:07:36 +0100

    Update commands.py

--- commands.py
+++ commands.py
@@ -4,8 +4,7 @@ from pyrx_imp import Ap, Db, Ed, Ge, Gi, Gs, Rx, Bim
 from helper import helper
 from helper import repo
 
-print("added command - bct_test")
-print("added command - bct_reloadmodules")
+print("added command - bct_reloadcommands")
 print("added command - bct_repoupdate")
 
 def OnPyReload() -> None:
@@ -14,12 +13,6 @@ def OnPyReload() -> None:
         importlib.reload(helper)
         importlib.reload(repo)
         print("\nReloading all modules")
-    except Exception as err:
-        print(err)
-
-def PyRxCmd_bct_test():
-    try:
-        print("roks")
     except Exception as err:
         print(err)
 
