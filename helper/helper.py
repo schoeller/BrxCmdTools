7c4b20e1d8a17a05ecc1952a76af3a2ca522c452
Sebastian <106036+schoeller@users.noreply.github.com>
2024-12-09 17:59:44 +0100

    Update helper.py

--- helper/helper.py
+++ helper/helper.py
@@ -1,10 +1,11 @@
 import traceback
 from pyrx_imp import Ap, Db, Ed, Ge, Gi, Gs, Rx, Bim
-import repo
+#import repo
 
 def reload_commands():
     try:
-        print(repo.get_repo_dir())
+        # print(repo.get_repo_dir())
+        print("ups")
 
     except Exception as e:
         print(f"Error displaying changes: {e}")
